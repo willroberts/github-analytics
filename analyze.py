db51ffb71c83bf663c17af88db23946db42b8df7
Will Roberts <willroberts3@protonmail.com>
2025-07-21 11:19:54 -0400

    Adds line counting

--- analyze.py
+++ analyze.py
@@ -11,9 +11,9 @@ class Language:
 @dataclass
 class Repository:
     name: str
-    stargazerCount: int
-    diskUsage: int
-    primaryLanguage: str
+    stars: int
+    disk_usage: int
+    primary_language: str
     languages: list[Language]
 
 def parse_data() -> list[Repository]:
@@ -29,51 +29,57 @@ def parse_data() -> list[Repository]:
         ) for d in data
     ]
 
-def count_languages(data: list, count: int) -> None:
-    occurrences = list()
-    for repo in data:
-        for lang in repo['languages']:
-            occurrences.append(lang['node']['name'])
-    counter = Counter(occurrences)
+def count_languages(repos: list[Repository], count: int = 10) -> None:
+    c = Counter([lang.name for repo in repos for lang in repo.languages])
     print('Most common programming languages:')
-    for lang in counter.most_common(count):
+    for lang in c.most_common(count):
         print(lang)
 
-def count_primary_languages(data: list, count: int) -> None:
-    occurrences = list()
-    for repo in data:
-        if repo['primaryLanguage'] is not None:
-            occurrences.append(repo['primaryLanguage']['name'])
-    counter = Counter(occurrences)
+def count_primary_languages(repos: list[Repository], count: int = 10) -> None:
+    c = Counter([r.primary_language for r in repos])
     print('Most common primary programming languages:')
-    for lang in counter.most_common(count):
+    for lang in c.most_common(count):
         print(lang)
 
-def find_language_usage(data: list, target_lang: str) -> None:
-    repos = list()
-    for repo in data:
-        for lang in repo['languages']:
-            if lang['node']['name'] == target_lang:
-                repos.append(repo['name'])
+def count_language_lines(repos: list[Repository], count: int = 10) -> None:
+    langs = [lang for repo in repos for lang in repo.languages]
+    c = Counter({lang.name: lang.lines for lang in langs})
+    print('Lines of code by language:')
+    for lang, lines in c.most_common(count):
+        print((lang, lines))
+
+def find_language_usage(repos: list[Repository], target_lang: str) -> None:
+    repos = [r.name for r in repos if r.primary_language == target_lang]
+    print(f'Usage of programming language {target_lang}:')
     for repo in repos:
         print(repo)
 
-def show_largest(data: list, count: int) -> None:
-    data = sorted(data, key=lambda r: r['diskUsage'], reverse=True)
-    if count < len(data):
-        data = data[:count]
-    print('Largest repos:')
-    for repo in data:
-        print((repo['name'], repo['diskUsage']))
+def show_largest(repos: list[Repository], count: int = 10) -> None:
+    repos = sorted(repos, key=lambda r: r.disk_usage, reverse=True)
+    if count < len(repos):
+        repos = repos[:count]
+    print('Largest repositories:')
+    for repo in repos:
+        print((repo.name, repo.disk_usage))
 
-def show_most_stars(data: list, count: int) -> None:
-    data = sorted(data, key=lambda r: r['stargazerCount'], reverse=True)
-    if count < len(data):
-        data = data[:count]
-    print('Most starred repos:')
-    for repo in data:
-        print((repo['name'], repo['stargazerCount']))
+def show_most_stars(repos: list[Repository], count: int = 10) -> None:
+    repos = sorted(repos, key=lambda r: r.stars, reverse=True)
+    if count < len(repos):
+        repos = repos[:count]
+    print('Most starred repositories:')
+    for repo in repos:
+        print((repo.name, repo.stars))
 
 if __name__ == '__main__':
-    l = parse_data()
-    print(len(l))
+    data = parse_data()
+    count_languages(data)
+    print()
+    count_primary_languages(data)
+    print()
+    count_language_lines(data)
+    print()
+    find_language_usage(data, 'Rust')
+    print()
+    show_largest(data)
+    print()
+    show_most_stars(data)
