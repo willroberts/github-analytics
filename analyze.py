cb6890b208022e8a954eebfbd76fa7f0891fe386
Will Roberts <willroberts3@protonmail.com>
2025-07-22 19:03:00 -0400

    Condenses options

--- analyze.py
+++ analyze.py
@@ -4,6 +4,11 @@ from collections import Counter
 from dataclasses import dataclass
 
 @dataclass
+class Options:
+    exclude_private: bool = False
+    exclude_forks: bool = False
+
+@dataclass
 class Language:
     name: str
     lines: int
@@ -33,30 +38,30 @@ def parse_repositories() -> list[Repository]:
         ) for d in data
     ]
 
-def count_languages(repos: list[Repository], exclude_private: bool = False, exclude_forks: bool = False, count: int = 10) -> None:
-    if exclude_private:
+def count_languages(repos: list[Repository], opts: Options, count: int = 10) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     c = Counter([lang.name for repo in repos for lang in repo.languages])
     print('Most common programming languages:')
     for lang in c.most_common(count):
         print(lang)
 
-def count_primary_languages(repos: list[Repository], exclude_private: bool = False, exclude_forks: bool = False, count: int = 10) -> None:
-    if exclude_private:
+def count_primary_languages(repos: list[Repository], opts: Options, count: int = 10) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     c = Counter([r.primary_language for r in repos])
     print('Most common primary programming languages:')
     for lang in c.most_common(count):
         print(lang)
 
-def count_language_lines(repos: list[Repository], exclude_private: bool = False, exclude_forks: bool = False, count: int = 10) -> None:
-    if exclude_private:
+def count_language_lines(repos: list[Repository], opts: Options, count: int = 10) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     langs = [lang for repo in repos for lang in repo.languages]
     c = Counter({lang.name: lang.lines for lang in langs})
@@ -64,20 +69,20 @@ def count_language_lines(repos: list[Repository], exclude_private: bool = False,
     for lang, lines in c.most_common(count):
         print((lang, lines))
 
-def find_language_usage(repos: list[Repository], target_lang: str, exclude_private: bool = False, exclude_forks: bool = False) -> None:
-    if exclude_private:
+def find_language_usage(repos: list[Repository], target_lang: str, opts: Options) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     repos = [repo.name for repo in repos for lang in repo.languages if lang.name == target_lang]
     print(f'Usage of programming language {target_lang}:')
     for repo in repos:
         print(repo)
 
-def show_largest(repos: list[Repository], exclude_private: bool = False, exclude_forks: bool = False, count: int = 10) -> None:
-    if exclude_private:
+def show_largest(repos: list[Repository], opts: Options, count: int = 10) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     repos = sorted(repos, key=lambda r: r.disk_usage, reverse=True)
     if count < len(repos):
@@ -86,10 +91,10 @@ def show_largest(repos: list[Repository], exclude_private: bool = False, exclude
     for repo in repos:
         print((repo.name, repo.disk_usage))
 
-def show_most_stars(repos: list[Repository], exclude_private: bool = False, exclude_forks: bool = False, count: int = 10) -> None:
-    if exclude_private:
+def show_most_stars(repos: list[Repository], opts: Options, count: int = 10) -> None:
+    if opts.exclude_private:
         repos = [r for r in repos if r.visibility != "PRIVATE"]
-    if exclude_forks:
+    if opts.exclude_forks:
         repos = [r for r in repos if not r.is_fork]
     repos = sorted(repos, key=lambda r: r.stars, reverse=True)
     if count < len(repos):
@@ -99,15 +104,16 @@ def show_most_stars(repos: list[Repository], exclude_private: bool = False, excl
         print((repo.name, repo.stars))
 
 if __name__ == '__main__':
+    opts = Options(exclude_private=True, exclude_forks=True)
     repos = parse_repositories()
-    count_languages(repos, exclude_private=True, exclude_forks=True)
+    count_languages(repos, opts)
     print()
-    count_primary_languages(repos, exclude_private=True, exclude_forks=True)
+    count_primary_languages(repos, opts)
     print()
-    count_language_lines(repos, exclude_private=True, exclude_forks=True)
+    count_language_lines(repos, opts)
     print()
-    find_language_usage(repos, 'Rust', exclude_private=True, exclude_forks=True)
+    find_language_usage(repos, 'Rust', opts)
     print()
-    show_largest(repos, exclude_private=True, exclude_forks=True)
+    show_largest(repos, opts)
     print()
-    show_most_stars(repos, exclude_private=True, exclude_forks=True)
+    show_most_stars(repos, opts)
